7be7a0fe239b3b0f7b63c92f26da65909ded27e8
prashant <prashant.prabhakar@raptorx.ai>
2025-05-19 17:18:04 +0530

    downgrade tailwind version, updated config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,10 @@
 // tailwind.config.js
-module.exports = {
+export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
